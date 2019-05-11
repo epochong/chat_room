c4a589e7debe7dfc7329b8efc9f57e0ba37bfc93
Valentineone <Valentineone@example.com>
2019-05-11 23:10:10 +0800

    chat room first update

---
+++ src/single/SingleThreadClient.java
@@ -0,0 +1,16 @@
+package single;
+
+/**
+ * @author wangchong
+ * @date 2019/5/10 19:22
+ * @email 876459397@qq.com
+ * @CSDN https://blog.csdn.net/wfcn_zyq
+ * @describe
+ */
+
+/**
+ * 单线程版
+ */
+public class SingleThreadClient {
+
+}
